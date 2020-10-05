3796852805f069e6bbe7df8fbc32ceba98d4c4ae
apple <apple@192.168.3.37>
2020-10-05 22:17:39 +0800

    Second version

--- CSE360/src/cse360assignment02/AddingMachine.java
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -1,27 +1,62 @@
+
+// my code repository : https://github.com/AgoodPersonYbr/cse360assignment02
+
+
 package cse360assignment02;
 public class AddingMachine {
+	
+	
+	public static void main(String[] args) {
+	       AddingMachine machine = new AddingMachine();
+	       machine.add(0);
+	       System.out.print(machine.toString());
+	       machine.add(4); 
+	       System.out.print(machine.toString());
+	       machine.subtract(2); 
+	       System.out.print(machine.toString());
+	       machine.add(5);
+	       System.out.print(machine.toString());
+	   }
+	
+	
 	 private int total;
+	 private String output; 
+	 
+	 
   
-  public AddingMachine () {
+  public AddingMachine() {
     total = 0;  // not needed - included for clarity
+    output = "0";
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  total = total + value;
+	  if(total == 0) {
+		  output = "0";
+	  }else
+		  
+	  output = " + " + value;
   }
 
   public void subtract (int value) {
+	  total = total + value;
+	  output = " - " + value;
   }
 
   public String toString () {
-    return "";
+    return output;
   }
 
   public void clear() {
+	  total = 0;
   }
 
 }
+
+
+
 
